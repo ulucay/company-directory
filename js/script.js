0735f86d54f384f0a7d50d520228549b81be403e
ulucay <31716850+ulucay@users.noreply.github.com>
2020-02-19 22:43:44 -0600

    Implements modals

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const url = 'https://randomuser.me/api/?results=12';
 const gallery = document.querySelector('#gallery');
-const card = document.querySelectorAll('.card');
+const modalContainer = document.querySelector('.modal-container');
+let peopleArray;
 
 fetch(url)
     .then(res => res.json())
     .then(data =>{
-        const people = data.results;
-        generatePeople(people);
+        peopleArray = data.results;
+        generatePeople(peopleArray);
+        generateModal(peopleArray);
+        
     })
 
 
@@ -22,14 +25,46 @@ function generatePeople(data){
             <div class="card-info-container">
                 <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
                 <p class="card-text">${person.email}</p>
-                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+                <p class="card-text cap">${person.location.city}, ${person.location.country}</p>
             </div>
         </div>
         `).join('');
 
         gallery.innerHTML = people;
+
+        let cards = document.querySelectorAll('.card');
+        
+        cards.forEach( card => card.addEventListener('click', e => console.log(e.target)))
+}
+
+function generateModal(data){
+    const modal = data.map( person => `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.phone}</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
+
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+
+    `).join('')
+
+    modalContainer.innerHTML = modal;
 }
 
 
-card.addEventListener("change", "click", (e) => console.log(e.target));
+
 
