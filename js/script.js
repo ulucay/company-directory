c1ff61d4100fbf3a5e4181e32e06e748c22d36a9
ulucay <clarkulucay@gmail.com>
2020-02-20 13:24:27 -0600

    cards pops up as a modal and close button is implemented

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const gallery = document.querySelector('#gallery');
 const modalContainer = document.querySelector('.modal-container');
 let peopleArray;
 
+//Fetchs data from API
 fetch(url)
     .then(res => res.json())
     .then(data =>{
@@ -13,12 +14,11 @@ fetch(url)
     })
 
 
-
-//HELPER FUNCTIONS
+//Generates people and inserts into HTML
 function generatePeople(data){
-       
-        const people = data.map( person => `
-        <div class="card">
+        //Creates card template according to fetched data, and stores inside a variable
+        const people = data.map( (person, index) => `
+        <div class="card" id="${index}">
             <div class="card-img-container">
                 <img class="card-img" src="${person.picture.large}" alt="profile picture">
             </div>
@@ -32,18 +32,24 @@ function generatePeople(data){
 
         gallery.innerHTML = people;
 
+        //Stores card elements from HTML
         let cards = document.querySelectorAll('.card');
         
-        cards.forEach( card => card.addEventListener('click', e => console.log(e.target)))
+        //If any cards clicked, then show the corresponding modal on the screen
+        cards.forEach( card => card.addEventListener('click', e => {
+            const cardId = e.currentTarget.id; // Get clicked card item's id
+            popUpModal(cardId);
+        }));
 }
 
+//Generates modals and insert into HTML
 function generateModal(data){
-    const modal = data.map( person => `
-    <div class="modal-container">
-        <div class="modal">
+    //Creates modal template according to fetched data, and stores inside a variable
+    const modal = data.map( (person, index) => `
+        <div class="modal" id="${index}">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
+                <img class="modal-img" src="${person.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">name</h3>
                 <p class="modal-text">${person.email}</p>
                 <p class="modal-text cap">${person.location.city}</p>
@@ -52,18 +58,40 @@ function generateModal(data){
                 <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
                 <p class="modal-text">Birthday: 10/21/2015</p>
             </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
         </div>
-
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>
-    </div>
-
-    `).join('')
+    `).join("")
 
     modalContainer.innerHTML = modal;
 }
+
+//Pops up clicked card as a modal
+function popUpModal(cardId){
+    let modals = document.querySelectorAll('.modal'); //Stores all modals
+    let closeButton = document.querySelectorAll('#modal-close-btn'); //Stores all close buttons
+
+    //If card is clicked, pop up the correct model by checking the id
+    modals.forEach((modal) => {
+        if(modal.id == cardId){
+            modalContainer.style.display = 'block';
+        }else{
+            modal.style.display = 'none';
+        }
+    });
+
+    //Adds features to buttons when user clicks, it modifys css of modal-container and modals
+    closeButton.forEach((button) => button.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+        modals.forEach((modal) =>{
+            modal.style.display = 'block'
+        })
+    }));
+}
+
+
 
 
 
